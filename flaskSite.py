2fe0004253e79f6adf30fa8c05b3b2f8acb67c33
Bogdan Gusuleac <bgusuleac@gmail.com>
2020-04-16 04:52:02 -0700

    2:51

---
+++ flaskSite.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, url_for, flash, redirect
+#from forms import RegistrationForm, LoginForm
+from secrets import token_hex
+from os import *
+
+#https://stackoverflow.com/questions/31002890/how-to-reference-a-html-template-from-a-different-directory-in-python-flask/31003097
+
+workingDirectory = path.dirname(__file__)
+workingDirectory = workingDirectory + "/Templates/Front-FoodTracker-master"
+print(workingDirectory)
+
+app = Flask(__name__, template_folder=workingDirectory)
+
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+
+# @app.route('/register', methods=['GET', 'POST'])
+# def register():
+#     form = RegistrationForm()
+#     if form.validate_on_submit():
+#         flash(f"account created for: {form.username.data}!", "success")
+#         return redirect(url_for('hello_world'))
+#         #use function name, not app_route
+#     return render_template('register.html', title='Register', form=form)
+#
+#
+# @app.route('/login')
+# def login():
+#     form = LoginForm()
+#     return render_template('login.html', title='Login', form=form)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
