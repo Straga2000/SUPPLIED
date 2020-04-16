6113a0fc4b742afcfb71f659cf19808ad7cc7d77
Bogdan Gusuleac <bgusuleac@gmail.com>
2020-04-16 11:57:33 -0700

    9:57

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from hashlib import *
 
 class siteFunctions:
     def __init__(self):
+
+        self.siteEmail = 'food.tracker.prices@gmail.com'
+        self.sitePassword = 'mqgjzndoxjhmlzbk'
+
         self.database = Database()
         self.database.create_collection("users")
         self.userList = {}
@@ -14,26 +18,35 @@ class siteFunctions:
 
     def login_user(self, email='', password_hash=''):
         id = self.security(password_hash)
-        self.userList[id] = self.get_user_data(id)
+
+        obj = self.database.query_one("users", {"id": id})
+        if obj is not None:
+            self.userList[id] = User()
+            self.userList[id].set_from_dict(obj)
+            return id
+        else:
+            return None
+
+    def logout_user(self, id=''):
+        self.userList[id] = None
 
     def add_user(self, first_name='', last_name='', email='', password_hash=''):
         id = self.security(password_hash)
         user = User(first_name, last_name, email, id, password_hash)
         self.database.insert_one_in_collection("users", user.to_dict())
 
-    def get_user_data(self, id = ''):
-        obj =  self.database.query_one("users", {"id": id})
-        if obj is not None:
-            user = User()
-            user.set_from_dict(obj)
-            return user
-        else:
-            return None
-
     def add_product(self, id, category, name, price, quantity, time):
-        user = self.get_user_data(id)
-        user.product_list.add_product(category, name, price, quantity, time)
+        self.userList[id].add_item(category, name, price, quantity, time)
 
     def delete_product(self, id, name):
-        user = self.get_user_data(id)
-        user.product_list.delete_product(name)
+        self.userList[id].product_list.delete_product(name)
+
+    def get_week_forecast(self, id):
+        self.userList[id].product_list.get_expense_over_a_week()
+
+    def get_month_forecast(self, id):
+        self.userList[id].product_list.get_expense_over_a_month()
+
+    def get_email_by_search(self, id, objList):
+        userEmail = self.userList[id].email
+        send_mail(objList, self.siteEmail, self.sitePassword, userEmail)
