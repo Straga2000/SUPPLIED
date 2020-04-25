6e0ea8c5c630aba5c4c7ff49e3c041d0b9278c0d
Johnny <Johnny.savu.99@gmail.com>
2020-04-26 00:15:04 +0300

    finished back-end

--- flaskSite.py
+++ flaskSite.py
@@ -5,13 +5,41 @@ from os import *
 from random import choice, randint
 from main import *
 
-time = 0
+#global time
+time = 4
+
+#product_list
+
+product_list = [
+    {"product": "Miere", "quantity":1, "forecast":7},
+    {"product": "Nuci", "quantity":2, "forecast":5},
+    {"product": "Alune", "quantity":1, "forecast":5},
+    {"product": "Ciocolata", "quantity":3, "forecast":10},
+    {"product": "Apa", "quantity":2, "forecast":1},
+    {"product": "Paine", "quantity":2, "forecast":1}
+]
+
+#daily_list
+
+#fiecare din urmatoarele lista contine un dictionar cu name, average_price si monthly_expense
+#si sunt liste cu produse cumparate zilnic, sapt, lunar
+print('-----------------------------------------------')
+daily_list = Site.get_items_list(Site.security("Bob"), 'daily') 
+weekly_list = Site.get_items_list(Site.security("Bob"), 'weekly')
+monthly_list = Site.get_items_list(Site.security("Bob"), 'monthly')
+
+#cele 3 expensuri pe care trebuie sa le pui in html
+daily_expense = Site.get_daily_forecast(Site.security("Bob"))
+weekly_expense = Site.get_week_forecast(Site.security("Bob"))
+monthly_expense = Site.get_month_forecast(Site.security("Bob"))
+
+
+print('-----------------------------------------------')
+
 
 #https://stackoverflow.com/questions/31002890/how-to-reference-a-html-template-from-a-different-directory-in-python-flask/31003097
 
 workingDirectory = path.dirname(__file__)
-#workingDirectory = workingDirectory + "/Templates/Front-FoodTracker-master"
-#print(workingDirectory)
 
 app = Flask(__name__, template_folder=workingDirectory)
 
@@ -21,21 +49,24 @@ posts = []
 for i in range(10):
     posts.append({"product": choice(productName), "quantity": randint(2, 30), "forecast": randint(5, 10)})
 
-print(posts)
+
 
 @app.route("/", methods =['GET','POST'])
 def hello_world():
-    return render_template("table.html", posts=posts)
+    return render_template("table.html", posts=product_list)
 
 
 @app.route("/post", methods =['GET','POST'])
 def worker():
     global time 
     global Site
+
+    # iti adauga 
     time += 1
     data = request.get_json()
     
     Site.add_product(Site.security("Bob"), "unknown", data['product'], float(data['price']), int(data['quantity']), time)
+    #print(Site.get_daily_forecast(Site.security("Bob")))
 
     if Site.get_days_left(Site.security("Bob"))[data['product']]:
 
@@ -47,11 +78,20 @@ def worker():
     else:
         data['forecast'] = "Not known yet"
 
-    posts.append(data)
     
-    print(Site.get_week_forecast(Site.security("Bob")))
+
+    product_list.append(data)
+
+    #print(Site.get_week_forecast(Site.security("Bob")))
 
     return redirect('/')
+
+
+if __name__ == "__main__":
+ 
+    app.run(debug=True, use_reloader=True)
+
+
 
 # @app.route('/register', methods=['GET', 'POST'])
 # def register():
@@ -67,8 +107,3 @@ def worker():
 # def login():
 #     form = LoginForm()
 #     return render_template('login.html', title='Login', form=form)
-
-if __name__ == "__main__":
-    #Site = siteFunctions()
-    app.run(debug=True, use_reloader=True)
-
